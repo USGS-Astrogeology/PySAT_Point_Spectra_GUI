4159e43a135a98e96c49d65a95f6afa004362cb2
Adam Paquette <acp263@nau.edu>
2018-01-24 15:29:01 -0700

    Removal of stack tracing.

--- tests/core/test_BaselineRemoval.py
+++ tests/core/test_BaselineRemoval.py
@@ -7,14 +7,6 @@ from PyQt5 import QtCore, QtWidgets
 from point_spectra_gui.util import PandasModel as pm
 from point_spectra_gui.core.BaselineRemoval import BaselineRemoval as BR
 
-import sys
-
-def trace(frame, event, arg):
-    print("{}, {}:{}".format(event, frame.f_code.co_filename, frame.f_lineno))
-    return trace
-
-sys.settrace(trace)
-
 @pytest.fixture
 def repeat_pandas_model(n):
     data =  np.repeat(np.arange(1, n + 1), (n)).reshape(n, -1)
