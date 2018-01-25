f66bb2d074e63da070302404555839e28fe79aeb
Adam Paquette <acp263@nau.edu>
2018-01-25 12:17:02 -0700

    Added testing for DataTable and updated cores init file.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,6 @@ import pandas as pd
 
 @pytest.fixture
 def repeat_df(n):
-    data =  np.repeat(np.arange(1, n + 1), (n)).reshape(n,-1)
-    columns = np.arange(1, n+1)
+    data =  np.repeat(np.arange(1, n + 1), (n)).reshape(n, -1)
+    columns = np.arange(1, n + 1)
     return pd.DataFrame(data, columns = columns)
---
+++ tests/core/test_data_table.py
@@ -0,0 +1,29 @@
+import pytest
+import numpy as np
+import pandas as pd
+
+from PyQt5 import QtCore, QtWidgets
+
+from point_spectra_gui.util.PandasModel import PandasModel as pm
+from point_spectra_gui.core import DataTable
+
+@pytest.fixture
+def repeat_df(n):
+    data =  np.repeat(np.arange(1, n + 1), (n)).reshape(n, -1)
+    columns = np.arange(1, n + 1)
+    df = pd.DataFrame(data, columns = columns)
+    return pm(df)
+
+def test_data_table(qtbot):
+    form = QtWidgets.QWidget()
+    guiDT = DataTable()
+    guiDT.setupUi(form)
+
+    guiDT.data = {'data_key': repeat_df(5)}
+
+    guiDT.chooseDataComboBox.addItem('data_key')
+    guiDT.chooseDataComboBox.setItemText(0, 'data_key')
+
+    guiDT.on_refreshTable()
+
+    assert type(guiDT.data['data_key']._data) == type(pd.DataFrame())
