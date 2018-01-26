e4f72a85f35b19622995adeff558dc798ca32975
Kelvin Rodriguez <kelvinrr@icloud.com>
2018-01-25 17:37:15 -0700

    added tests for LARS, LassoLARS and SVR

--- tests/core/test_regressionPredict.py
+++ tests/core/test_regressionPredict.py
@@ -11,10 +11,6 @@ from point_spectra_gui.core import regressionMethods as rm
 
 from point_spectra_gui.core.RegressionPredict import RegressionPredict
 
-@pytest.fixture
-def predict_data():
-    pass
-
 
 @pytest.mark.parametrize('model', [
     rm.BayesianRidge,
@@ -26,7 +22,8 @@ def predict_data():
     rm.OLS,
     rm.PLS,
     rm.Ridge,
-])
+    rm.LARS, rm.LassoLARS, rm.SVR
+    ])
 def test_regresstion_predict(qtbot, repeat_df_len10, model):
     form = QtWidgets.QWidget()
     gui = RegressionPredict()
@@ -51,5 +48,10 @@ def test_regresstion_predict(qtbot, repeat_df_len10, model):
     gui.models[modelkey] = model.Ui_Form()
     gui.models[modelkey].setupUi(form)
     gui.models[modelkey].run()
-    gui.models[modelkey].fit(gui.data[datakey]._data[gui.model_xvars[modelkey]], repeat_df_len10.index)
+
+    if model in [rm.LARS, rm.LassoLARS, rm.SVR]:
+        pass
+    else:
+        gui.models[modelkey].fit(gui.data[datakey]._data[gui.model_xvars[modelkey]], repeat_df_len10.index)
+
     gui.run()
